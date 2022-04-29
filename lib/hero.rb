93ae8b3e4207f3cf5aa10acb75a2d516ab420361
vinicius.padilha <vinicius.padilha@euax.com.br>
2022-04-29 11:58:53 -0300

    Criated test to check if Health's hero is equal or greather than 1000

--- lib/hero.rb
+++ lib/hero.rb
@@ -1,7 +1,8 @@
 class Hero
-    attr_accessor :weapon
+    attr_accessor :weapon, :health
 
     def initialize
         @weapon = 'sword'
+        @health = 1000
     end
 end
--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 require 'hero'
 
+#Describe = "Grupo de exemplos"
+#Pode receber um nome de classe ou uma string como parâmetros
 describe Hero do
     it 'has a sword' do
         hero = Hero.new
         expect(hero.weapon).to eq('sword')
+    end
+
+    it 'has more than 1000 HP points' do
+        #Irá criar um novo atribuindo a classe instanciada
+        hero = Hero.new
+        #Espera que a 'health' = vida do herói, seja maior ou igual a 1000
+        expect(hero.health).to be >= (1000)
     end
 end
